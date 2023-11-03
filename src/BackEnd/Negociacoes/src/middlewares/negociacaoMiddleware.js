4e878f36ed860da82bb22857e010397a715e902c
GeovaneVLG <61693060+GeovaneVLG@users.noreply.github.com>
2023-11-02 23:21:56 -0300

    Update negociacaoMiddleware.js
    Melhorado validações

--- src/BackEnd/Negociacoes/src/middlewares/negociacaoMiddleware.js
+++ src/BackEnd/Negociacoes/src/middlewares/negociacaoMiddleware.js
@@ -1,31 +1,35 @@
 const validateBody = (req, res, next) => {
     const { body } = req;
 
-    if (body.cliente_produtor === undefined || body.cliente_produtor === '') {
+    if (body.cliente_produtor === undefined || body.cliente_produtor === '' || body.cliente_produtor === null) {
         res.status(400).json({ message: 'Campo "cliente_produtor" é obrigatório' });
         return;
     }
-    if (body.tipo_operacao === undefined || body.tipo_operacao === '') {
+    if (body.tipo_operacao === undefined || body.tipo_operacao === '' || body.tipo_operacao === null) {
         res.status(400).json({ message: 'Campo "tipo_operacao" é obrigatório' });
         return;
     }
-    if (body.valor_por_saca === undefined || body.valor_por_saca === '') {
+    if (body.valor_por_saca === undefined || body.valor_por_saca === '' || body.valor_por_saca === null) {
         res.status(400).json({ message: 'Campo "valor_por_saca" é obrigatório' });
         return;
     }
-    if (body.quantidade_saca === undefined || body.quantidade_saca === '') {
+    if (body.quantidade_saca === undefined || body.quantidade_saca === '' || body.quantidade_saca === null) {
         res.status(400).json({ message: 'Campo "quantidade_saca" é obrigatório' });
         return;
     }
-    if (body.data_vencimento === undefined || body.data_vencimento === '') {
+    if (body.data_vencimento === undefined || body.data_vencimento === '' || body.data_vencimento === null) {
         res.status(400).json({ message: 'Campo "data_vencimento" é obrigatório' });
         return;
     }
-    if (body.valor_total === undefined || body.valor_total === '') {
+    if (body.valor_total === undefined || body.valor_total === 0 || body.valor_total === null) {
         res.status(400).json({ message: 'Campo "valor_total" é obrigatório' });
         return;
     }
-    if (body.unidade === undefined || body.unidade === '') {
+    if (body.unidade === undefined || body.unidade === '' || body.unidade === null) {
+        res.status(400).json({ message: 'Campo "unidade" é obrigatório' });
+        return;
+    }
+    if (body.id_usuario === undefined || body.id_usuario === '' || body.id_usuario === null) {
         res.status(400).json({ message: 'Campo "unidade" é obrigatório' });
         return;
     }
